4c1a0fd0e6fa51d09d7656cede48b8cb5c9e940e
jaychenfe <chenjiehwy@163.com>
2020-03-19 16:39:04 +0800

    完成项目初始化

---
+++ foodie_dev_api/src/main/java/com/github/jaychenfe/Application.java
@@ -0,0 +1,14 @@
+package com.github.jaychenfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jaychenfe
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}

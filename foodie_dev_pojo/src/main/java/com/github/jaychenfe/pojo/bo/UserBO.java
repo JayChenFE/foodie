e0ef8c7c133dccb5580201c772a7b95ef3d5365d
JayChenFE <jaychenfe@foxmail.com>
2020-03-21 10:19:42 +0800

    接入swagger2
    接入swagger2

--- foodie_dev_pojo/src/main/java/com/github/jaychenfe/pojo/bo/UserBO.java
+++ foodie_dev_pojo/src/main/java/com/github/jaychenfe/pojo/bo/UserBO.java
@@ -1,9 +1,19 @@
 package com.github.jaychenfe.pojo.bo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * @author jaychenfe
+ */
+@ApiModel(value = "用户对象BO", description = "从客户端,由用户传入的数据封装")
 public class UserBO {
 
+    @ApiModelProperty(value = "用户名", required = true)
     private String username;
+    @ApiModelProperty(value = "密码", required = true)
     private String password;
+    @ApiModelProperty(value = "确认密码", required = true)
     private String confirmPassword;
 
     public String getUsername() {

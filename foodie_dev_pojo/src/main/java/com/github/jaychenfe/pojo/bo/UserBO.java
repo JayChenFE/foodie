9bb49c3de209fece337a2717ccd36f56f98ea60b
jaychenfe <chenjiehwy@163.com>
2020-03-23 11:51:10 +0800

    升级mysql驱动

--- foodie_dev_pojo/src/main/java/com/github/jaychenfe/pojo/bo/UserBO.java
+++ foodie_dev_pojo/src/main/java/com/github/jaychenfe/pojo/bo/UserBO.java
@@ -2,12 +2,10 @@ package com.github.jaychenfe.pojo.bo;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
 
 /**
  * @author jaychenfe
  */
-@Data
 @ApiModel(value = "用户对象BO", description = "从客户端,由用户传入的数据封装")
 public class UserBO {
 
@@ -17,4 +15,32 @@ public class UserBO {
     private String password;
     @ApiModelProperty(value = "确认密码", required = true)
     private String confirmPassword;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getConfirmPassword() {
+        return this.confirmPassword;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String toString() {
+        return "UserBO(username=" + this.getUsername() + ", password=" + this.getPassword() + ", confirmPassword=" + this.getConfirmPassword() + ")";
+    }
 }

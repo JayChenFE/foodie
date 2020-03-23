5da7cecfe452c81d3f93a91251fcf97107c176e5
jaychenfe <chenjiehwy@163.com>
2020-03-23 17:49:33 +0800

    完成根据三级分类搜索

--- foodie_dev_api/src/main/java/com/github/jaychenfe/controller/ItemsController.java
+++ foodie_dev_api/src/main/java/com/github/jaychenfe/controller/ItemsController.java
@@ -99,10 +99,7 @@ public class ItemsController extends BaseController {
             pageSize = COMMON_PAGE_SIZE;
         }
 
-        PagedGridResult grid = itemService.queryPagedComments(itemId,
-                level,
-                page,
-                pageSize);
+        PagedGridResult grid = itemService.queryPagedComments(itemId, level, page, pageSize);
 
         return ApiResponse.ok(grid);
     }
@@ -112,11 +109,11 @@ public class ItemsController extends BaseController {
     public ApiResponse search(
             @ApiParam(name = "keywords", value = "关键字", required = true)
             @RequestParam String keywords,
-            @ApiParam(name = "sort", value = "排序", required = false)
+            @ApiParam(name = "sort", value = "排序")
             @RequestParam String sort,
-            @ApiParam(name = "page", value = "查询下一页的第几页", required = false)
+            @ApiParam(name = "page", value = "查询下一页的第几页")
             @RequestParam Integer page,
-            @ApiParam(name = "pageSize", value = "分页的每一页显示的条数", required = false)
+            @ApiParam(name = "pageSize", value = "分页的每一页显示的条数")
             @RequestParam Integer pageSize) {
 
         if (StringUtils.isBlank(keywords)) {
@@ -131,10 +128,36 @@ public class ItemsController extends BaseController {
             pageSize = PAGE_SIZE;
         }
 
-        PagedGridResult grid = itemService.searchItems(keywords,
-                sort,
-                page,
-                pageSize);
+        PagedGridResult grid = itemService.searchItems(keywords, sort, page, pageSize);
+
+        return ApiResponse.ok(grid);
+    }
+
+    @ApiOperation(value = "通过分类id搜索商品列表", notes = "通过分类id搜索商品列表", httpMethod = "GET")
+    @GetMapping("/catItems")
+    public ApiResponse catItems(
+            @ApiParam(name = "catId", value = "三级分类id", required = true)
+            @RequestParam Integer catId,
+            @ApiParam(name = "sort", value = "排序")
+            @RequestParam String sort,
+            @ApiParam(name = "page", value = "查询下一页的第几页")
+            @RequestParam Integer page,
+            @ApiParam(name = "pageSize", value = "分页的每一页显示的条数")
+            @RequestParam Integer pageSize) {
+
+        if (catId == null) {
+            return ApiResponse.errorMsg(null);
+        }
+
+        if (page == null) {
+            page = 1;
+        }
+
+        if (pageSize == null) {
+            pageSize = PAGE_SIZE;
+        }
+
+        PagedGridResult grid = itemService.searchItems(catId, sort, page, pageSize);
 
         return ApiResponse.ok(grid);
     }

54044dc528c0610e30bacb28297068051ef49797
jaychenfe <chenjiehwy@163.com>
2020-03-24 17:27:23 +0800

    完成收货地址相关接口

---
+++ foodie_dev_service/src/main/java/com/github/jaychenfe/service/AddressService.java
@@ -0,0 +1,56 @@
+package com.github.jaychenfe.service;
+
+import com.github.jaychenfe.pojo.UserAddress;
+import com.github.jaychenfe.pojo.bo.AddressBO;
+
+import java.util.List;
+
+public interface AddressService {
+
+    /**
+     * 根据用户id查询用户的收货地址列表
+     *
+     * @param userId 用户id
+     * @return 用户的收货地址列表
+     */
+    List<UserAddress> queryAll(String userId);
+
+    /**
+     * 用户新增地址
+     *
+     * @param addressBO addressBO
+     */
+    void addNewUserAddress(AddressBO addressBO);
+
+    /**
+     * 用户修改地址
+     *
+     * @param addressBO addressBO
+     */
+    void updateUserAddress(AddressBO addressBO);
+
+    /**
+     * 根据用户id和地址id，删除对应的用户地址信息
+     *
+     * @param userId    用户id
+     * @param addressId 地址id
+     */
+    void deleteUserAddress(String userId, String addressId);
+
+    /**
+     * 修改默认地址
+     *
+     * @param userId    用户id
+     * @param addressId 地址id
+     */
+    void updateUserAddressToBeDefault(String userId, String addressId);
+
+    /**
+     * 根据用户id和地址id，查询具体的用户地址对象信息
+     *
+     * @param userId    用户id
+     * @param addressId 地址id
+     * @return 地址
+     */
+    UserAddress queryUserAddress(String userId, String addressId);
+}

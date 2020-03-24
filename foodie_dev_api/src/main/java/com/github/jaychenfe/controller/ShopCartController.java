cd23602332c24464acf5be7bad0e1df67da371a2
jaychenfe <chenjiehwy@163.com>
2020-03-24 16:09:30 +0800

    完成从购物车删除商品接口

--- foodie_dev_api/src/main/java/com/github/jaychenfe/controller/ShopCartController.java
+++ foodie_dev_api/src/main/java/com/github/jaychenfe/controller/ShopCartController.java
@@ -42,4 +42,22 @@ public class ShopCartController {
 
         return ApiResponse.ok();
     }
+
+    @ApiOperation(value = "从购物车中删除商品", notes = "从购物车中删除商品")
+    @PostMapping("/del")
+    public ApiResponse del(
+            @RequestParam String userId,
+            @RequestParam String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)) {
+            return ApiResponse.errorMsg("参数不能为空");
+        }
+
+        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
+
+        return ApiResponse.ok();
+    }
 }
